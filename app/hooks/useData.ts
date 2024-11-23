620f542285b5b7d0b635fa4885866488470c8e81
Morymirco <morykoulibaly996@gmail.com>
2024-11-23 13:22:57 +0000

    'nnnn'

--- app/hooks/useData.ts
+++ app/hooks/useData.ts
@@ -1,7 +1,14 @@
 import useSWR from 'swr'
+import { getLocations } from '@/app/services/firebase/locations'
+import type { Location } from '@/app/types/location'
+
+// Définition du fetcher qui utilise notre fonction getLocations
+const fetcher = async (): Promise<Location[]> => {
+  return await getLocations()
+}
 
 export function useLocations() {
-  const { data, error } = useSWR('/api/locations', fetcher, {
+  const { data, error } = useSWR<Location[]>('locations', fetcher, {
     revalidateOnFocus: false,
     revalidateOnReconnect: false,
     dedupingInterval: 60000
